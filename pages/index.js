82d39490e4f99548568de2a4827216428788cb07
thethirdsh <62771092+thethirdsh@users.noreply.github.com>
2023-02-26 21:41:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Index = () => (
   <div className="mt-8 max-w-xl mx-auto px-8">
     <h1 className="text-center">
       <span className="block text-xl text-gray-600 leading-tight">
-        Welcome to
+        Momen Yasser 2001627 Welcomes you to
       </span>
       <span className="block text-5xl font-bold leading-none">
         Cloud Computing Course
